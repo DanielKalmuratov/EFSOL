9aed9d772c11f1f094291269a295715915536f2b
Daniel <danielkalmuratov@gmail.com>
2021-05-28 17:15:14 +0600

    adding new file

---
+++ C#basic/Books/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Books
+{
+    class Program
+    {
+
+        class Book {
+
+            public class Notes {
+                public string Note;
+                public Notes(string note) {
+                    Note = note;
+                }
+                
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Book.Notes note = new Book.Notes("first note");
+            Console.WriteLine(note.Note);
+            note.Note = "second note";
+            Console.WriteLine(note.Note);
+            Console.ReadKey();
+        }
+    }
+}
