fd309185d4cd19307d5edd412eabc46e6050fa15
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-05-28 11:02:14 +0600

    add solutions to lesson 10-11

---
+++ C#basic/CarCollection/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CarCollection
+{
+    interface ICar
+    {
+        string Name { get; set; }
+        int Year { get; set; }
+
+    }
+    class CarCollection<T> where T: ICar, new()
+    {
+        private T[] Cars;
+        private int current = 0;
+        public int Count { get { return Cars == null ? 0 : Cars.Length; } }
+
+        public void Add(string Name, int Year)
+        {
+            if (Cars == null)
+                Cars = new T[1];
+            if (Cars.Length == current)
+                Array.Resize(ref Cars, Cars.Length + 1);
+            Cars[current] = new T() { Name = Name, Year = Year };
+            current++;
+        }
+
+        public T this[int Index]
+        {
+            get { return Cars[Index]; }
+            set { Cars[Index] = value; }
+        }
+    }
+    class Car : ICar
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public override string ToString()
+        {
+            return $"Car : {Name} {Year}";
+        }
+
+    }
+        class Program
+    {
+        static void Get<T>() where T : ICar, new()
+        {
+            var collection = new CarCollection<T>();
+            collection.Add("AUDI", 2021);
+            collection.Add("BMW", 2012);
+
+            Console.WriteLine("Collection length: {0}", collection.Count);
+            for (int i = 0; i < collection.Count; i++)
+                Console.WriteLine("[{0}]: {1}",i ,collection[i]);
+        }
+        static void Main(string[] args)
+        {
+
+                Get<Car>();
+                Console.ReadKey();
+        }
+    }
+}
---
+++ C#basic/MyList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyList
+{
+   
+    public class MyList<T>
+    {
+        private T[] array;
+        private int current = 0;
+        public int Count { get { return array == null ? 0 : array.Length; } }
+        public void Add(T value)
+        {
+            if (array == null)
+                array = new T[1];
+            if (array.Length == current)
+                Array.Resize(ref array, array.Length + 1);
+            array[current] = value;
+            current++;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> array = new MyList<int>();
+            array.Add(1);
+            array.Add(3);
+            Console.WriteLine("Индекс [0] = " + array[0]);
+            Console.WriteLine("Индекс [1] = " + array[1]);
+            Console.WriteLine("Array.Length: " + array.Count);
+            Console.ReadKey();
+        }
+    }
+}
