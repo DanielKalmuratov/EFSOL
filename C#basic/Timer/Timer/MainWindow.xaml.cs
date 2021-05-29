db8196e5dbf593f1043d356c834e541e7834942f
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-05-29 19:57:44 +0600

    add Timer task for lesson 11

--- C#basic/Timer/Timer/MainWindow.xaml.cs
+++ C#basic/Timer/Timer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Timer
 {
@@ -20,14 +21,41 @@ namespace Timer
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+        DateTime date;
         public MainWindow()
         {
             InitializeComponent();
+            
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            date = DateTime.Now;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(10);
+            timer.Tick += timer_Tick;
+            timer.Start();
 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            date = DateTime.Now;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(10);
+            timer.Tick -= timer_Tick;
+            timer.Start();
+            timer.Stop();
+            
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            long tick = DateTime.Now.Ticks - date.Ticks;
+            DateTime stopWatch = new DateTime();
+            stopWatch = stopWatch.AddTicks(tick);
+            Timer.Content = String.Format("{0:HH:mm:ss:ff}", stopWatch);
         }
     }
 }
