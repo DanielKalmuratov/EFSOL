ae48a38546defe1947a8de0686867370aeadb339
Daniel <danielkalmuratov@gmail.com>
2021-06-02 17:59:12 +0600

    add dictionary class

--- C#basic/MatrixFallString/MatrixFallString/Program.cs
+++ C#basic/MatrixFallString/MatrixFallString/Program.cs
@@ -37,54 +37,59 @@ namespace MatrixFallString
         }
         static void Main(string[] args)
         {
-            char[,] str = new char[15, 15];
-            int posY = 15;
-            int posX = 15;
-
-
-            Random rnd = new Random();
-
-            for (int i = 0; i < posY; i++)
-            {
-                for (int j = 0; j < posY; j++)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    str[j, i] = (char)rnd.Next(0x0410, 0x44F);
-                    Console.Write(str[j,j]);
-                }
-            }
-
-
-            Console.ReadKey();
-
-
-
-
-
-
+            //char[,] str = new char[15, 15];
+            //int posY = 15;
+            //int posX = 15;
 
 
             //Random rnd = new Random();
-            //for (; ; ) {
 
-            //    for (int i = 1; i <= 7; i++) {
-            //        for (int j = 1; j <= 8; j++) {
-
-            //            if (rnd.Next(10) > 5)
-            //            {
-            //                Console.ForegroundColor = ConsoleColor.Green;
-            //            }
-            //            else {
-            //                Console.ForegroundColor = ConsoleColor.DarkGreen;
-            //            }
-            //            int n = rnd.Next(2);
-            //            Console.Write(n);
-            //            if (j != 7) {
-            //                Console.Write("");
-            //            }
-            //        }
+            //for (int i = 0; i < posY; i++)
+            //{
+            //    for (int j = 0; j < posY; j++)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Green;
+            //        str[j, i] = (char)rnd.Next(0x0410, 0x44F);
+            //        Console.Write(str[j,j]);
             //    }
             //}
+
+
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+            Random rnd = new Random();
+            for (; ; )
+            {
+
+                for (int i = 1; i <= 7; i++)
+                {
+                    for (int j = 1; j <= 8; j++)
+                    {
+
+                        if (rnd.Next(10) > 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        }
+                        int n = rnd.Next(2);
+                        Console.Write(n);
+                        if (j != 7)
+                        {
+                            Console.Write("");
+                        }
+                    }
+                }
+            }
 
 
 
---
+++ C#basic/MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        class MyDictionary<TKey, TValue> {
+            List<TKey> tkey;
+            List<TValue> tvalue;
+            private int counter = 0;
+            private int current = 0; 
+            
+            public int Counter
+            {
+                get { return this.counter; }
+            }
+
+            public MyDictionary()
+            {
+                this.tkey = new List<TKey>(1);
+                this.tvalue = new List<TValue>(1);
+                counter = 1;
+
+            }
+            public TValue this[TKey key] {
+                get { return tvalue[GetInd(key)]; }
+            }
+
+            private int GetInd(TKey key)
+            {
+                for (int itm = 0; itm < tkey.Count; itm++) {
+                    if ($"{tkey[itm]}" == $"{key}")
+                        return itm;
+                };
+                return -1;
+            }
+
+            public void Add(TKey key, TValue value)
+            {
+                tkey.Add(key);
+                tvalue.Add(value);
+                this.counter = tvalue.Count;
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "Book");
+            Console.WriteLine(dictionary[1]);
+            Console.ReadKey();
+            Dictionary<int, string> dfd = new Dictionary<int, string>();
+        }
+    }
+}
