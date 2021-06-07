75aeeb48dc925aa8f4008f5ecc8ae6a526b6cb01
Daniel <danielkalmuratov@gmail.com>
2021-06-07 16:12:26 +0600

    adding solution of lesson 10

---
+++ C#basic/StructWorker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StructWorker
+{
+    class Program
+    {
+        struct Worker {
+            string fullName;
+            string position;
+            int startYear;
+        }
+        static void Main(string[] args)
+        {
+            Worker[,] arrOfWorker = new Worker[5,3];
+            Console.WriteLine("Please enter StudentId, StudentName, CourseName, Date-Of-Birth");
+            string fullname = Console.ReadLine();
+            string position = Console.ReadLine();
+            c_name = Console.ReadLine();
+            
+        }
+    }
+}
---
+++ C#углубленный/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    abstract class Education {
+
+        public void TemplateMethod() {
+            Enter();
+            Study();
+            Exams();
+            Graduation();
+        }
+
+        public abstract void Enter();
+        public abstract void Study();
+        public abstract void Exams();
+        public abstract void Graduation();
+    }
+    class School : Education {
+        public override void Enter()
+        {
+            Console.WriteLine("Поступление в Школу");
+        }
+        public override void Study()
+        {
+            Console.WriteLine("Обучение в Школе");
+        }
+        public override void Exams()
+        {
+            Console.WriteLine("Сдача экзаменов.");
+        }
+        public override void Graduation()
+        {
+            Console.WriteLine("Выпускной");
+        }
+    }
+    class University : Education {
+        public override void Enter()
+        {
+            Console.WriteLine("Поступление в Универ");
+        }
+        public override void Study()
+        {
+            Console.WriteLine("Обучение в Универе");
+        }
+        public override void Exams()
+        {
+            Console.WriteLine("Сдача экзаменов.");
+        }
+        public override void Graduation()
+        {
+            Console.WriteLine("Выпускной");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            University univer = new University();
+            School school = new School();
+
+
+            school.TemplateMethod();
+            Console.WriteLine(new string('-', 20));
+            univer.TemplateMethod();
+            
+            Console.ReadKey();
+
+        }
+    }
+}
