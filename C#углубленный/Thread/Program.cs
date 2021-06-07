c63d0f125cb079c14053d11d829c98db0ca92c5d
Daniel <danielkalmuratov@gmail.com>
2021-06-07 18:57:03 +0600

    add new proj threads, need to finish task

---
+++ C#углубленный/Thread/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream1 = new FileStream("file1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //var stream2 = new FileStream("file2.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);    
+            //var stream3 = new FileStream("file3.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            
+            var sw1 = new StreamWriter(stream1);
+            sw1.WriteLine("SOME INFORMATION1");
+            var sr1 = new StreamReader(stream1);
+            Console.WriteLine(sr1.ReadToEnd());
+            Console.ReadKey();
+
+
+            
+
+
+
+
+
+            stream1.Close();
+            //stream2.Close();
+            //stream3.Close();
+
+        }
+    }
+}
