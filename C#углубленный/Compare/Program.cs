2f8e471fbe628868f41c4e0d85737224d8f59aaf
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-06-09 00:52:46 +0600

    add new solu-s of lesson 2 and 11

---
+++ C#углубленный/Compare/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace Compare
+{
+    class Program
+    {
+        public class Car { 
+            public string name { set; get; }
+
+            public Car(string n) {
+                this.name = n;
+            }
+
+            public override int GetHashCode()
+            {
+                return name.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                var nCar = obj as Car;
+                if (nCar == null) {
+                    return false;
+                }
+                return nCar.name == name;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var cars = new OrderedDictionary();
+            var car1 = new Car("Audi");
+            var car2 = new Car("Mazda");
+            cars.Add(car1, "626");
+            cars.Add(car2, "A4");
+            
+            try 
+            {
+                cars.Add(car1, "fds");
+            } 
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+            foreach (DictionaryEntry item in cars)
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- C#углубленный/Thread/Program.cs
+++ C#углубленный/Thread/Program.cs
@@ -1,33 +1,48 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 
-namespace Thread
+namespace ReadWrite
 {
     class Program
     {
+        static readonly StreamReader str1 = File.OpenText("file1.txt");
+        static readonly StreamReader str2 = File.OpenText("file2.txt");
+        static readonly StreamWriter str3 = File.CreateText("file3.txt");
+        public static object blok = new object();
+        static void Read1()
+        {
+            string str = str1.ReadToEnd();
+            str1.Close();
+            Console.WriteLine("{0} - содержимое первого файла", str);
+            lock (blok)
+            {
+                str3.WriteLine(str);
+            }
+        }
+        static void Read2()
+        {
+            string str = str2.ReadToEnd();
+            str1.Close();
+            Console.WriteLine("{0} - содержимое второго файла", str);
+            lock (blok)
+            {
+                str3.WriteLine(str);
+            }
+        }
         static void Main(string[] args)
         {
-            var stream1 = new FileStream("file1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            //var stream2 = new FileStream("file2.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);    
-            //var stream3 = new FileStream("file3.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            
-            var sw1 = new StreamWriter(stream1);
-            sw1.WriteLine("SOME INFORMATION1");
-            var sr1 = new StreamReader(stream1);
-            Console.WriteLine(sr1.ReadToEnd());
+            Thread[] array = new Thread[] { new Thread(Read2), new Thread(Read1) };
+
+            for (int i = 0; i < array.Length; i++) {
+                array[i].Start();
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Join();
+            }
             Console.ReadKey();
-
-
-            
-
-
-
-
-
-            stream1.Close();
-            //stream2.Close();
-            //stream3.Close();
-
+            str3.Close();
         }
     }
 }
