6e643fd405c00ec1268e01349241ef8d0bcbe367
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-06-09 05:14:25 +0600

    add new task for lesson 12

---
+++ C#углубленный/TaskMutex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace TaskMutex
+{
+    class Program
+    {
+        static Mutex mutexObj = new Mutex();
+        static int x = 0;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++) {
+                Thread thread = new Thread(Start);
+                thread.Name = $"Thread name-{i}";
+                thread.Start();
+                Thread.Sleep(1000);
+            }
+            Console.ReadKey();
+        }
+        public static void Start() {
+            mutexObj.WaitOne();
+            x = 1;
+            for (int i = 0; i < 6; i++) {
+                Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
+                x++;
+                
+            }
+            mutexObj.ReleaseMutex();
+        }
+    }
+}
