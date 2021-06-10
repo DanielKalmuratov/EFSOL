5576a600d3889d9f54f15ec037cc0a622de45c85
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-06-10 18:44:32 +0600

    add last task

--- C#basic/MyDictionary/MyDictionary/Program.cs
+++ C#basic/MyDictionary/MyDictionary/Program.cs
@@ -4,75 +4,78 @@ using System.Collections.Generic;
 
 namespace MyDictionary
 {
-    class Program
+    public class MyDictionary<TKey, TValue> : IEnumerator, IEnumerable
     {
-        class MyDictionary<TKey, TValue> : IEnumerator, IEnumerable
+        List<TKey> tkey;
+        List<TValue> tvalue;
+        private int counter = 0;
+
+        public int Count
         {
-            List<TKey> tkey;
-            List<TValue> tvalue;
-            private int counter = 0;
-          
-            public int Count
-            {
-                get { return this.counter; }
-            }
+            get { return this.counter; }
+        }
 
-            public MyDictionary()
-            {
-                this.tkey = new List<TKey>(1);
-                this.tvalue = new List<TValue>(1);
-                counter = 1;
-
-            }
-            public TValue this[TKey key] {
-                get { return tvalue[GetInd(key)]; }
-            }
-
-            private int GetInd(TKey key)
-            {
-                for (int itm = 0; itm < tkey.Count; itm++) {
-                    if ($"{tkey[itm]}" == $"{key}")
-                        return itm;
-                };
-                return -1;
-            }
-
-            int position = -1;
-
-            bool IEnumerator.MoveNext()
-            {
-                if (position < tvalue.Count - 1)
-                {
-                    position++;
-                    return true;
-                }
-                ((IEnumerator)this).Reset();
-                return false;
-            }
-            void IEnumerator.Reset()
-            {
-                position = -1;
-            }
-            object IEnumerator.Current
-            {
-                get { return tvalue[position]; }
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return (IEnumerator)this;
-            }
-
-            public void Add(TKey key, TValue value)
-            {
-                tkey.Add(key);
-                tvalue.Add(value);
-                this.counter = tvalue.Count;
-            }
-
-
+        public MyDictionary()
+        {
+            this.tkey = new List<TKey>(1);
+            this.tvalue = new List<TValue>(1);
+            counter = 1;
 
         }
+        public TValue this[TKey key]
+        {
+            get { return tvalue[GetInd(key)]; }
+        }
+
+        private int GetInd(TKey key)
+        {
+            for (int itm = 0; itm < tkey.Count; itm++)
+            {
+                if ($"{tkey[itm]}" == $"{key}")
+                    return itm;
+            };
+            return -1;
+        }
+
+        int position = -1;
+
+        bool IEnumerator.MoveNext()
+        {
+            if (position < tvalue.Count - 1)
+            {
+                position++;
+                return true;
+            }
+            ((IEnumerator)this).Reset();
+            return false;
+        }
+        void IEnumerator.Reset()
+        {
+            position = -1;
+        }
+        object IEnumerator.Current
+        {
+            get { return tvalue[position]; }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            tkey.Add(key);
+            tvalue.Add(value);
+            this.counter = tvalue.Count;
+        }
+
+
+
+    }
+    class Program
+    {
+        
         static void Main(string[] args)
         {
             MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
---
+++ C#basic/usingLesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MyDictionary;
+
+namespace usingLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "Book");
+            dictionary.Add(2, "student");
+            dictionary.Add(3, "food");
+            dictionary.Add(4, "water");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
