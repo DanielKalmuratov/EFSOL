f25cc4095d7c7501e1e55be57d9dbd980293ee58
Daniel <danielkalmuratov@gmail.com>
2021-06-10 15:01:29 +0600

    last commit

---
+++ C#углубленный/AsyncProgram/AsyncProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            Action action = new Action(Method);
+            AsyncCallback callback = new AsyncCallback(CallBack);
+
+            Console.ReadKey();
+            action.BeginInvoke(callback, new object());
+            
+        }
+        static void Method() {
+
+            for (int i = 0; i < 100; i++) {
+                Thread.Sleep(1000);
+                Console.Write("2");
+            }
+            Console.WriteLine("Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+
+        }
+        static void CallBack(IAsyncResult asyncResult)
+        {
+            Console.WriteLine("Thread id: {0}",Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ C#углубленный/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            Action action = new Action(Method);
+            AsyncCallback callback = new AsyncCallback(CallBack);
+
+            Console.ReadKey();
+            action.BeginInvoke(callback, "some string");
+
+        }
+        static void Method()
+        {
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write(".");
+            }
+            Console.WriteLine("Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+        static void CallBack(IAsyncResult asyncResult)
+        {
+            Console.WriteLine("Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
