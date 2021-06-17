0a6c250361deb82ff0e72ac613cc26f5479125d2
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-06-18 01:49:22 +0600

    reflection task

---
+++ C#углубленный/AssembTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssembTask
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assemb = Assembly.Load("Temprerature");
+            dynamic temp = Activator.CreateInstance(assemb.GetType("Temprerature.Temperature"));
+            Console.WriteLine("{1}°C = {0}°F",temp.ToFahrenheit(5m), 5);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#углубленный/FileReadWrite/Temprerature/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Temprerature
+{
+    public class Temperature
+    {
+        public decimal ToFahrenheit(decimal temperature)
+        {
+            return temperature * 9 / 5 + 32;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
