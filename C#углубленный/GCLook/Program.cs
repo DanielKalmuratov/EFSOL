17a9b8d05d27a57bc5d019aa4d2010076d1065e8
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-06-20 23:32:21 +0600

    add task TPL

---
+++ C#углубленный/GCLook/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace GCLook
+{
+    class LookResource
+    {
+        readonly int limit;
+
+        public LookResource(int limit)
+        {
+            this.limit = limit;
+        }
+
+        bool IsOverLimit()
+        {
+            return this.limit < GC.GetTotalMemory(false);
+        }
+
+        public void ExceptWarning(object err)
+        {
+            if (IsOverLimit())
+            {
+                Console.WriteLine("{0}", err);
+            }
+        }
+
+    }
+
+    class LargeObject
+    {
+        int[] array = new int[50000000]; 
+
+        public void Method(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Timer timer = new Timer(new LookResource(10000000).ExceptWarning, "Warning", 0, 500);
+
+            LargeObject[] array = new LargeObject[800];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                new LargeObject().Method(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#углубленный/TPL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL
+{
+    class Program
+    {
+        static void Method1() {
+            Console.WriteLine("Method 1");
+            for (int i = 0; i < 10; i++) {
+                Thread.Sleep(200);
+                Console.Write("\nCount method1: {0} ", i);
+            }
+        }
+        static void Method2()
+        {
+            Console.WriteLine("Method 2");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write("\nCount method2: {0} ", i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method start!");
+            Parallel.Invoke(Method1, Method2);
+            Console.WriteLine("\nMain Method finish!");
+            Console.ReadKey();
+        }
+        
+    }
+}
