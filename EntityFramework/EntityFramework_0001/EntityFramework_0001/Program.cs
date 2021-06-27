d7913584c1ac895b087d7e0692313f049f18c227
Daniel Kalmuratov <danielkalmuratov@gmail.com>
2021-06-28 03:22:21 +0600

    Entity framework 1st lesson

---
+++ EntityFramework/EntityFramework_0001/EntityFramework_0001/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_0001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDbEntities myDbEntities = new MyDbEntities();
+            List<Employee> employees = myDbEntities.Employee.ToList();
+            List<EmpFamily> empFam = myDbEntities.EmpFamily.ToList();
+            List<EmployeeInfo> empInfo = myDbEntities.EmployeeInfo.ToList();
+
+            foreach (Employee emp in employees) 
+            {
+                Console.WriteLine("Name:       {0}", emp.FirstName);
+                Console.WriteLine("Phone:      {0}", emp.PhoneNumber);
+                Console.WriteLine("------------------------------------------------");
+            }
+            foreach (EmpFamily empFamily in empFam)
+            {
+                Console.WriteLine("Birth date: {0}", empFamily.Birthdate);
+                Console.WriteLine("Married:    {0}", empFamily.Married);
+                Console.WriteLine("Address:    {0}", empFamily.Address1);
+                Console.WriteLine("------------------------------------------------");
+            }
+            foreach (EmployeeInfo empInf in empInfo)
+            {
+                Console.WriteLine("Salary:     {0}", empInf.Salary);
+                Console.WriteLine("Position:   {0}", empInf.Position);
+                Console.WriteLine("------------------------------------------------");
+            }
+
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
